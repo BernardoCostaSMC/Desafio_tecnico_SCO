bd9ad3499d43e9597e4587a7e91c6d4dc7678ae2
BernardoCostaSMC <103154036+BernardoCostaSMC@users.noreply.github.com>
2026-02-27 21:42:39 -0300

    style tela

--- todo/forms.py
+++ todo/forms.py
@@ -5,3 +5,18 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'status']
+        
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite o título da tarefa'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a descrição',
+                'rows': 3
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+        }
